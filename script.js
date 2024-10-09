36eed12cd81e1f90310b1e689db958ea847b5f84
AnderKitty <135187474+AnderKitty@users.noreply.github.com>
2024-10-08 21:34:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,68 @@
 const cards = document.querySelectorAll('.card');
+const starField = document.querySelector('.star-field');
+const twinklingStars = document.querySelector('.twinkling-stars');
+const numStars = 100;
+
+function handleCardMovement(e) {
+    const card = e.currentTarget;
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const ratioX = (x / rect.width) * 2 - 1;
+    const ratioY = (y / rect.height) * 2 - 1;
+
+    card.style.setProperty('--ratio-x', ratioX);
+    card.style.setProperty('--ratio-y', ratioY);
+}
+
+function resetCardPosition(e) {
+    const card = e.currentTarget;
+    card.style.setProperty('--ratio-x', 0);
+    card.style.setProperty('--ratio-y', 0);
+}
 
 cards.forEach(card => {
-    card.addEventListener('mousemove', (e) => {
-        const rect = card.getBoundingClientRect();
-        const x = e.clientX - rect.left; // Posición X del mouse en la tarjeta
-        const y = e.clientY - rect.top; // Posición Y del mouse en la tarjeta
+    card.addEventListener('mousemove', handleCardMovement);
+    card.addEventListener('mouseleave', resetCardPosition);
+});
 
-        // Normalizamos las posiciones de X e Y
-        const ratioX = (x / rect.width) * 2 - 1; // Valor entre -1 y 1
-        const ratioY = (y / rect.height) * 2 - 1; // Valor entre -1 y 1
+function createStar() {
+    const star = document.createElement('div');
+    star.classList.add('star');
+    
+    const size = Math.random() * 3;
+    const x = Math.random() * 100;
+    const y = Math.random() * 100;
+    const delay = Math.random() * 2;
 
-        // Actualiza las variables CSS para la rotación
-        card.style.setProperty('--ratio-x', ratioX);
-        card.style.setProperty('--ratio-y', ratioY); // No invertir Y para rotación correcta
+    Object.assign(star.style, {
+        width: `${size}px`,
+        height: `${size}px`,
+        left: `${x}vw`,
+        top: `${y}vh`,
+        animationDelay: `${delay}s`
     });
+    
+    return star;
+}
 
-    card.addEventListener('mouseleave', () => {
-        card.style.setProperty('--ratio-x', 0); // Resetea la rotación al salir
-        card.style.setProperty('--ratio-y', 0);
-    });
+function populateStarField() {
+    const fragment = document.createDocumentFragment();
+    for (let i = 0; i < numStars; i++) {
+        fragment.appendChild(createStar());
+    }
+    twinklingStars.appendChild(fragment);
+}
+// Función para mostrar las estrellas fugaces después de 2.6 segundos
+function showShootingStars() {
+    setTimeout(() => {
+        starField.classList.add('visible');
+    }, 2460);
+}
+
+// Llamar a las funciones cuando se carga la página
+document.addEventListener('DOMContentLoaded', () => {
+    populateStarField();
+    showShootingStars();
 });
